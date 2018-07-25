6e3468c54f4634201d400bd02abe6dd51cd4ab00
kham300 <boogaga3000@gmail.com>
2018-07-25 17:12:29 +0300

    проблемы с базой надо чтобы при нажатии на фаворит срабатывал только один раз 2

---
+++ app/src/main/java/com/example/h_mamytov/newflickrphotogallery/Adapters/CustomAdapter.java
@@ -0,0 +1,97 @@
+package com.example.h_mamytov.newflickrphotogallery.Adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.h_mamytov.newflickrphotogallery.Utils.BlockingLifoQueue;
+import com.example.h_mamytov.newflickrphotogallery.Utils.DownloadManager;
+import com.example.h_mamytov.newflickrphotogallery.entity.MyData;
+import com.example.h_mamytov.newflickrphotogallery.R;
+import com.example.h_mamytov.newflickrphotogallery.data.OpenDBHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+
+    private List<MyData> myDataList;
+    private ThreadPoolExecutor threadPoolExecutor ;
+
+    public CustomAdapter() {
+        this.myDataList = new ArrayList<>();
+        threadPoolExecutor = new ThreadPoolExecutor(5, 5, 1, TimeUnit.MINUTES, new BlockingLifoQueue<Runnable>());
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.all_recent_photos, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, int i) {
+        final MyData data = myDataList.get(i);
+        if (data.isFavorite()){
+           viewHolder.button.setImageResource(R.drawable.ic_star_black_24dp);
+        }
+        viewHolder.imageView.setImageBitmap(null);
+        viewHolder.imageView.setTag(data.getUrl());
+        threadPoolExecutor.execute(new DownloadManager(viewHolder.imageView, data.getUrl(), i));
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return myDataList.size();
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView textView;
+        private ImageButton button;
+
+        public ViewHolder(@NonNull final View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.card_image);
+            textView = itemView.findViewById(R.id.card_name);
+            button = itemView.findViewById(R.id.favorite_item);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    MyData myData = CustomAdapter.this.myDataList.get(getAdapterPosition());
+                    if (!myData.isFavorite()){
+                        button.setImageResource(R.drawable.ic_star_black_24dp);
+                        OpenDBHelper instance = OpenDBHelper.getInstance();
+                        instance.insertFavoritePhotos(myData);
+                        myData.setFavorite(true);
+
+                    } else {
+                        button.setImageResource(R.drawable.ic_star_border_black_24dp);
+                        OpenDBHelper instance = OpenDBHelper.getInstance();
+                        instance.deleteFavoritePhotoById(myData.getId());
+                        myDataList.remove(myData);
+                    }
+                }
+            });
+
+        }
+
+    }
+
+    public void setMyDataList(List<MyData> myDataList) {
+        this.myDataList = myDataList;
+    }
+}
---
+++ app/src/main/java/com/example/h_mamytov/newflickrphotogallery/Fragments/FragmentHome.java
@@ -0,0 +1,68 @@
+package com.example.h_mamytov.newflickrphotogallery.Fragments;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.h_mamytov.newflickrphotogallery.Adapters.CustomAdapter;
+import com.example.h_mamytov.newflickrphotogallery.Utils.FlickrFetchr;
+import com.example.h_mamytov.newflickrphotogallery.entity.MyData;
+import com.example.h_mamytov.newflickrphotogallery.R;
+
+import java.util.List;
+
+public class FragmentHome extends Fragment {
+
+    private CustomAdapter adapter;
+    private Handler handler;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+
+        initData();
+
+        StaggeredGridLayoutManager staggeredGridLayoutManager =
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+
+        adapter = new CustomAdapter();
+        recyclerView.setAdapter(adapter);
+
+        handler = new Handler();
+
+        return view;
+    }
+
+    public void initData(){
+        Thread thread = new Thread(new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void run() {
+                List<MyData> myData = FlickrFetchr.downloadGalleryItems();
+                adapter.setMyDataList(myData);
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        });
+        thread.start();
+    }
+}
